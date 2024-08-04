a28f109665c07c89e6cd70a77059aafde99cb7a3
yeon <ryuyeon0403@gmail.com>
2024-08-04 17:01:27 +0900

    card component clone

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
 export default function Home() {
-  return <main />;
+  return (
+    <main className="bg-gray-400 h-screen flex items-center justify-center p-5">
+      <div className="bg-white w-full shadow-lg p-5 rounded-2xl">
+        <div>
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col">
+              <span className="text-sm text-gray-600 font-semibold -mb-1">
+                In transit
+              </span>
+              <span className="text-4xl font-semibold">Coolblue</span>
+            </div>
+            <div className="size-12 bg-orange-400 rounded-full" />
+          </div>
+          <div className="my-2 flex items-center gap-2">
+            <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full">
+              Today
+            </span>
+            <span>9:30~10:30AM</span>
+          </div>
+          <div className="relative">
+            <div className="absolute bg-gray-200 w-full h-2 rounded-full" />
+            <div className="absolute bg-green-500 w-2/3 h-2 rounded-full" />
+          </div>
+          <div className="flex justify-between items-center mt-5 text-gray-600">
+            <span>Expected</span>
+            <span>Sorting center</span>
+            <span>In transit</span>
+            <span className="text-gray-400">Delivered</span>
+          </div>
+        </div>
+        <div />
+      </div>
+    </main>
+  );
 }
